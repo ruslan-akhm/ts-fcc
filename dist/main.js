74308392c74d64fa207d48c029206ede188ae71c
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-06-09 11:11:46 -0400

    setup

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+var user = {
+    name: "Ruslan",
+    age: 27
+};
+var user2 = {
+    name: "Andy"
+};
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+interface User {
+  name: string;
+  age?: number;
+}
+
+const user: User = {
+  name: "Ruslan",
+  age: 27,
+};
+
+const user2: User = {
+  name: "Andy",
+};
