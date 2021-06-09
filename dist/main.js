3e113b09d92103e33a37ab3dea3d00940dd1d32f
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-06-09 11:16:29 -0400

    interfaces

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,13 @@
 var user = {
     name: "Ruslan",
-    age: 27
+    age: 27,
+    getMessage: function () {
+        return "Hello" + name;
+    }
 };
 var user2 = {
-    name: "Andy"
+    name: "Andy",
+    getMessage: function () {
+        return "Hello" + name;
+    }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-interface User {
+interface UserInterface {
   name: string;
   age?: number;
+  getMessage(): string;
 }
 
-const user: User = {
+const user: UserInterface = {
   name: "Ruslan",
   age: 27,
+  getMessage() {
+    return "Hello" + name;
+  },
 };
 
-const user2: User = {
+const user2: UserInterface = {
   name: "Andy",
+  getMessage() {
+    return "Hello" + name;
+  },
 };
